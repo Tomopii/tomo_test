abb5083d9353158b647f158162f9841ac7512fb8
tomo <tomo@little-studios.co.jp>
2021-12-23 16:02:23 +0900

    consoleつけた

--- test/musakui.js
+++ test/musakui.js
@@ -110,6 +110,7 @@ var PerformanceHarness = {
         window.setTimeout(addNextCase, tDelay);
         S_sansei_n = Math.floor(Math.random()*(sansei_n/(hantai_n + sansei_n)) * tSampleSize);
         S_hantai_n = tSampleSize - S_sansei_n;
+        console.log(this_.trialNum+"/"+S_sansei_n+"/"+S_hantai_n);
         this.codapPhone.call({
           action: 'createCase',
           args: {
